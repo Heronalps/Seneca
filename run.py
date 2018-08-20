e1d96a26016d93fcc2e4115d991fd2b6f886a0d3
Michael_Zhang <heronalps@gmail.com>
2018-08-20 11:50:44 -0700

    Add async combination for celery and lambda in run.py

--- run.py
+++ run.py
@@ -8,38 +8,58 @@ lambda_sync = False
 invokeTime = 12
 
 if (celery_sync and lambda_sync):
-    for num in range(5):
+    total_time = 0
+    for num in range(invokeTime):
+        print("Lambda is invoked %d time" %(num))
+        timestamp_start = time.time()
         response = invoke_sync.delay('')
-        print("Lambda is invoked %d time" %(num))
         result = response.get()
+        total_time += time.time() - timestamp_start
         print(result)
+    print("Time spent : ", total_time)
+
 elif (celery_sync and not lambda_sync):
-    for num in range(5):
-        response = invoke_async.delay('')
+    total_time = 0
+    for num in range(invokeTime):
         print("Lambda is invoked %d time" %(num))
+        timestamp_start = time.time()
+        response = invoke_async.delay('')
         result = response.get()
+        total_time += time.time() - timestamp_start
         print(result)
-# elif (not celery_sync and lambda_sync):
-#     # job = group(add.s(1,1), add.s(2,2))()
-#     job = group(invoke_sync.s('') for i in range(invokeTime))
-#     print(job)
-#     result = job.apply_async()
-#     print(result)
-#     print(result.ready())
-#     print(result.successful())
-#     while not result.ready():
-#         print("Retry")
-#         r = result.join()
-#         # time.sleep(1)
-#     print(r)
+    # Pull all responses from DynamoDB
 
+    print("Time spent : ", total_time)
 elif (not celery_sync and lambda_sync):
-    args = [''] * invokeTime
-    pool = ThreadPool(4)
-    sync_results = pool.map(invoke_sync.delay, args)
-    pool.close()
-    pool.join()
-    print(sync_results)
+    # job = group(add.s(1,1), add.s(2,2))()
+    job = group(invoke_sync.s('') for i in range(invokeTime))
+    timestamp_start = time.time()
+    result = job.apply_async()
+    r = result.join()
+    timestamp_complete = time.time()
+    print("Time Spent : ", timestamp_complete - timestamp_start)
+    print(r)
 
-    for res in sync_results:
-        print(res.get())
+# # In this version, the multithreading is provided by python, not celery 
+# elif (not celery_sync and lambda_sync):
+#     args = [''] * invokeTime
+#     pool = ThreadPool(4)
+#     sync_results = pool.map(invoke_sync.delay, args)
+#     pool.close()
+#     pool.join()
+#     print(sync_results)
+
+#     for res in sync_results:
+#         print(res.get())
+
+elif (not celery_sync and not lambda_sync):
+    job = group(invoke_async.s('') for i in range(invokeTime))
+    timestamp_start = time.time()
+    result = job.apply_async()
+    r = result.join()
+    timestamp_complete = time.time()
+    print("Time Spent : ", timestamp_complete - timestamp_start)
+    print(r)
+    # Pull the result from DynamoDB
+    
+

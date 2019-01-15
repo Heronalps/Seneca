d192ce465028f0ace3556c8fc758796ccda38fe0
Michael Zhang <heronalps@gmail.com>
2019-01-15 00:03:50 -0800

    mz - update requirements.txt

--- run_prophet.py
+++ run_prophet.py
@@ -61,27 +61,27 @@ def grid_search_controller():
     max_metric = float('inf')
     chosen_model_event = None
 
-    for payload in payload_list:
-        map_item = grid_search_worker(payload)
-        if map_item['average_metric'] < max_metric:
-            print ("======Update chosen model event==========")
-            chosen_model_event = map_item['event']
+    # for payload in payload_list:
+    #     map_item = grid_search_worker(payload)
+    #     if map_item['average_metric'] < max_metric:
+    #         print ("======Update chosen model event==========")
+    #         chosen_model_event = map_item['event']
     
-    # job = group(invoke_lambda.s(
-    #                 function_name = LAMBDA_NAME,
-    #                 sync = True,
-    #                 payload = payload
-    #                 ) for payload in payload_list)
-    # print("===Async Tasks start===")
-    # result = job.apply_async()
-    # result.join_native(timeout=None)
-    # model_list = result.get()
-    # print("===Async Tasks end===")
-    # # import pdb; pdb.set_trace();
-    # for item in model_list:
-    #     payload = item['Payload']
-    #     if payload['average_metric'] < max_metric:
-    #         chosen_model_event = payload['event']
+    job = group(invoke_lambda.s(
+                    function_name = LAMBDA_NAME,
+                    sync = True,
+                    payload = payload
+                    ) for payload in payload_list)
+    print("===Async Tasks start===")
+    result = job.apply_async()
+    result.join_native(timeout=None)
+    model_list = result.get()
+    print("===Async Tasks end===")
+    # import pdb; pdb.set_trace();
+    for item in model_list:
+        payload = item['Payload']
+        if payload['average_metric'] < max_metric:
+            chosen_model_event = payload['event']
     
     # # Non-zero forecast period makes lambda upload graphs to s3
     # chosen_model_event['forecast'] = FORECAST

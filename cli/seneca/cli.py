c6bda70cfc5b137adabdd6a3d4b57bfb5140225d
Michael Zhang <heronalps@gmail.com>
2019-02-09 21:14:18 -0800

    mz - update cli.py

--- cli/seneca/cli.py
+++ cli/seneca/cli.py
@@ -11,20 +11,15 @@ from src.allocated_memory.optimizer import Optimizer
 import click, subprocess
 
 @click.command()
-@click.option('--config_path', '-c', required=True,
-                                     help='Path to hyperparameter config file',
-                                     default='./config/prophet/config.py')
-@click.option('--lambda_path', '-l', required=True,
-                                     help='Path to lambda handler',
-                                     default='./src/lambda_func/prophet/prophet.py')
+@click.option('--config_path', '-c', required=False,
+                                     help='Path to hyperparameter config file')
+@click.option('--lambda_path', '-l', required=False,
+                                     help='Path to lambda handler')
 @click.option('--model', '-m', required=True, 
                                #prompt='Specified model',
                                help='The specified model',
-                               type = click.Choice(['prophet',
-                                             'XGBoost',
-                                             'multi_regression',
-                                             'svc',
-                                             'neural_network']))
+                               type = click.Choice(['prophet', 'XGBoost', 'multi_regression', 
+                                                    'svc', 'neural_network']))
 @click.option('--rebuild', '-b', is_flag=True, default=False, 
                                  help='The lambda package will be rebuilt if true')
 @click.option('--optimize', '-o', is_flag=True, default=False,
@@ -34,41 +29,33 @@ import click, subprocess
 def main(config_path, lambda_path, model, rebuild, optimize, repeat):
     click.echo("=============Seneca==============")
     click.echo('The specified model is {0}'.format(model))
-    click.echo('Config Path is at {0}'.format(config_path))
-    click.echo('Lambda Path is at {0}'.format(lambda_path))
-    
-    # Rename lambda handler and move to corresponding folder /src/lambda_func/
-
-    # commands = "mv {0} {1}; mv {1} {2};".format(lambda_path, 
-    #                                             model + '.py',
-    #                                             seneca_path + '/src/lambda_func/' + model)
-    
-    # p = subprocess.Popen(commands, shell=True)
-    # p.wait()
 
     # Both optimization or rebuild need deployment
     if optimize or rebuild:
             auto_deploy(model)
             if optimize:
                 auto_optimize(model)
-    print ("Repeat invocation {0} times!".format(repeat))
+    else:
+        click.echo('Config Path is at {0}'.format(config_path))
+        click.echo('Lambda Path is at {0}'.format(lambda_path))
+        print ("Repeat invocation {0} times!".format(repeat))
 
-    for n in range(repeat):
-        print ("Calling {0} for {1} times!".format(model, n + 1))
-        if model == 'prophet':
-            prophet(config_path, run_prophet)
+        for n in range(repeat):
+            print ("Calling {0} for {1} times!".format(model, n + 1))
+            if model == 'prophet':
+                prophet(config_path, run_prophet)
 
-        elif model == 'multi_regression':
-            multi_regression(config_path, run_multi_regression)
+            elif model == 'multi_regression':
+                multi_regression(config_path, run_multi_regression)
 
-        elif model == 'XGBoost':
-            xgboost(config_path, run_xgboost)
+            elif model == 'XGBoost':
+                xgboost(config_path, run_xgboost)
 
-        elif model == 'neural_network':
-            neural_network(config_path, run_neural_network)
-        
-        elif model == 'svc':
-            svc(config_path, run_svc)
+            elif model == 'neural_network':
+                neural_network(config_path, run_neural_network)
+            
+            elif model == 'svc':
+                svc(config_path, run_svc)
 
 def auto_deploy(model):
     my_env = os.environ.copy()
